4d6ec7caf99dbfe7225184e90c50883f9f7f2842
Mayank Agarwal <agarwal.mayank32@gmail.com>
2016-11-11 18:24:42 +0530

    Button Images Added to Drawable

---
+++ app/src/main/java/com/example/powerpack/audiorecordingtry/RecordingActivity_With2Buttons.java
@@ -0,0 +1,13 @@
+package com.example.powerpack.audiorecordingtry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordingActivity_With2Buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording__with2_buttons);
+    }
+}
